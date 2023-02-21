992ad5ca161f44a7778df5388fe843a35aaa516d
Sacuray <311721@niuitmo.ru>
2023-02-21 16:06:50 +0300

    web_4

---
+++ web_4/frontend/src/components/AuthorDetailes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AuthorDetailes = () => {
+  return <div className='detailes'>
+    <label className='author name'>Orekhov Sergey Vladimirovich</label><br/>
+    <label className='author group'>P32102</label><br/>
+    <label className='author variant'>Variant: 15155</label><br/>
+  </div>;
+};
+
+export default AuthorDetailes;
